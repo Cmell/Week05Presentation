654fa01b7fa820948848b31463b936a8a54246bf
Cmell <clmellinger@gmail.com>
2019-09-25 23:09:14 -0600

    trying php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="" xml:lang="">
+  <head>
+    <title>Lab Week 05</title>
+    <meta charset="utf-8" />
+    <meta name="author" content="Chris Mellinger" />
+  </head>
+  
+  <script>
+    window.location="LabWeek05_V01.html";
+  </script>
